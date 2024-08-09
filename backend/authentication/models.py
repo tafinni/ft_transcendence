7915d7d8f1e35bac2d0990ad9a579c56145ee2ca
AnastasiyaKuzmenko <42268839+AnastasiyaKuzmenko@users.noreply.github.com>
2024-08-09 13:30:46 +0300

    working on profile

--- backend/authentication/models.py
+++ backend/authentication/models.py
@@ -9,3 +9,15 @@ class UserProfile(models.Model):
 
     def __str__(self):
         return self.user.username
+
+# Signal to create or update user profile
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+
+@receiver(post_save, sender=User)
+def create_or_update_user_profile(sender, instance, created, **kwargs):
+    if created:
+        UserProfile.objects.create(user=instance)
+    else:
+        UserProfile.objects.get_or_create(user=instance)
+    instance.userprofile.save()
--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -137,4 +137,3 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
