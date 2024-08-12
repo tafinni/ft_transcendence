c25c3fedd26d2c521ef85e57e1e2f3c7a43011ff
Riku Leskinen <rleskine@student.hive.fi>
2024-08-12 19:16:32 +0300

    changed frontend docker from python to node, added game idle animation as page background, need to fix login and register

---
+++ frontend/src/js/profile.js
@@ -0,0 +1,70 @@
+
+export async function loadProfile() {
+
+	const response = await fetch('http://localhost:8000/profile/', {
+		method: 'GET',
+		credentials: 'include'
+	});
+	if (!response.ok) { console.error('Failed loading profile:', response.statusText); return `<h1>Error loading profile</h1>`; }
+
+	const userData = await response.json();
+
+	return `
+		<div class="container mt-5">
+			<div class="row">
+				<div class="col-md-4">
+					<div class="card text-center">
+						<div class="card-body">
+							<img src="{userData.avatar}" class="rounded-circle img-fluid mb-3" alt="Avatar" style="width: 150px; height: 150px;">
+							<h4 class="card-title">{userData.display_name}</h4>
+							<p class="text-muted">@${userData.username}</p>
+						</div>
+					</div>
+				</div>
+				<div class="col-md-8">
+					<div class="card">
+						<div class="card-body">
+							<h4 class="card-title">Personal Info</h4>
+							<table class="table table-striped">
+								<tbody>
+									<tr>
+										<th scope="row">First Name</th>
+										<td>${userData.first_name}</td>
+									</tr>
+									<tr>
+										<th scope="row">Last Name</th>
+										<td>${userData.last_name}</td>
+									</tr>
+									<tr>
+										<th scope="row">Display Name</th>
+										<td>{userData.display_name}</td>
+									</tr>
+									<tr>
+										<th scope="row">Username</th>
+										<td>${userData.username}</td>
+									</tr>
+									<tr>
+										<th scope="row">Email</th>
+										<td>{userData.email}</td>
+									</tr>
+								</tbody>
+							</table>
+							<h4 class="card-title mt-4">Change Password</h4>
+							<form id="change-password-form">
+								<div class="form-group">
+									<label for="current_password">Current Password</label>
+									<input type="password" class="form-control" id="current_password" placeholder="Enter current password" required>
+								</div>
+								<div class="form-group">
+									<label for="new_password">New Password</label>
+									<input type="password" class="form-control" id="new_password" placeholder="Enter new password" required>
+								</div>
+								<button type="submit" class="btn btn-primary">Change Password</button>
+							</form>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	`;
+}
---
+++ frontend/src/js/router.js
@@ -0,0 +1,80 @@
+import { loadHome } from './home.js';
+import { loadStats } from './stats.js';
+import { loadLogIn, initializeLogIn } from './login.js';
+import { loadRegister, initializeRegister } from './register.js';
+import { loadProfile } from './profile.js';
+import { updateContent } from './i18n.js';
+
+function navLinkVisibility(state) {
+	const homeLink = document.getElementById('home-link');
+	const profileLink = document.getElementById('profile-link');
+	const statsLink = document.getElementById('stats-link');
+	const loginLink = document.getElementById('login-link');
+
+	if (state) {
+		homeLink.style.display = 'block';
+		profileLink.style.display = 'block';
+		statsLink.style.display = 'block';
+		loginLink.style.display = 'block';
+	}
+	else {
+		homeLink.style.display ='none';
+		profileLink.style.display ='none';
+		statsLink.style.display ='none';
+		loginLink.style.display ='none';
+	}
+}
+
+// Update content
+export async function loadContent(content) {
+	const contentElement = document.getElementById('content');
+
+	if (content === 'home') {
+		contentElement.innerHTML = await loadHome();
+		navLinkVisibility(true);
+	}
+	else if (content === 'stats') {
+		contentElement.innerHTML = await loadStats();
+	}
+	else if (content === 'login') {
+		contentElement.innerHTML = loadLogIn();
+		initializeLogIn();
+		navLinkVisibility(false);
+	}
+	else if (content === 'register') {
+		contentElement.innerHTML = loadRegister();
+		initializeRegister();
+		navLinkVisibility(false);
+
+	}
+	else if (content === 'profile') {
+		contentElement.innerHTML = await loadProfile();
+	}
+	else {
+		contentElement.innerHTML = `<h1> 404 Page not found</h1>`;
+	}
+	updateContent();
+}
+
+// Navigation events
+document.getElementById('home-link').addEventListener('click', (event) => {
+	event.preventDefault(); // Stops normal link
+	loadContent('home');
+});
+document.getElementById('stats-link').addEventListener('click', (event) => {
+	event.preventDefault(); // Stops normal link
+	loadContent('stats');
+});
+document.getElementById('login-link').addEventListener('click', (event) => {
+	event.preventDefault(); // Stops normal link
+	loadContent('login');
+});
+document.getElementById('profile-link').addEventListener('click', (event) => {
+	event.preventDefault(); // Stops normal link
+	loadContent('profile');
+});
+
+/* Default content */
+window.onload = () => {
+	loadContent('login');
+}
