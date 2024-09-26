80f2f1df5bfb8956cff55fcdd815836ef6beeb1e
Taru Finni <120325749+tafinni@users.noreply.github.com>
2024-09-26 17:53:08 +0300

    Fixed setting to debug false

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -24,13 +24,34 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-xe3l)tqso(5e^3n2ql8(b#f=ia7*wgj)2_$ur@l!$ue9k=imzz'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    'localhost',
+    '127.0.0.1',
+    'localhost:1443',
 
-# Allow all origins #testing
-CORS_ALLOW_ALL_ORIGINS = True #testing
+]
 
+#CORS_ALLOW_ALL_ORIGINS = True # ONLY FOR TESTING
+
+CSRF_COOKIE_SECURE = True
+SESSION_COOKIE_SECURE = True
+CSRF_COOKIE_SAMESITE = 'None'
+SESSION_COOKIE_SAMESITE = 'None'
+
+# Allowed origins
+CORS_ALLOWED_ORIGINS = [
+	"https://localhost",
+    "https://localhost:1443",
+]
+
+CORS_ALLOW_CREDENTIALS = True
+
+CSRF_TRUSTED_ORIGINS = [
+    "https://localhost",
+    "https://localhost:1443",
+]
 
 # Application definition
 
