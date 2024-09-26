8e985ca43b29cbbcfc7dfb5c70cdf887594bc2b5
Taru Finni <120325749+tafinni@users.noreply.github.com>
2024-09-26 16:13:46 +0300

    Corrected game file

--- frontend/src/js/game.js
+++ frontend/src/js/game.js
@@ -1,134 +1,227 @@
-//this is in root dir
-import './css/styles.css'
 import * as THREE from 'three'
-//import * as THREE from "../node_modules/three/build/three.module.js"
-import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
-//import GUI from 'lil-gui'
+import gsap from 'gsap'
 
-/**
- * Base
- */
-// Debug
-//const gui = new GUI()
+import { loadContent } from './router.js'
+import * as t from './game.defs.js'
+import * as i from './idle.js'
+import * as s from './solo.js'
+import * as l2 from './local-2p.js'
+import * as l4 from './local-4p.js'
+import { updateContent } from "./i18n";
+import { showAlert } from './index.js'
 
-// Canvas
-const canvas = document.querySelector('canvas.webgl')
+// Variable to track current mode
+let m = i
 
-// Scene
-const scene = new THREE.Scene()
-
-/**
- * Textures
- */
-const textureLoader = new THREE.TextureLoader()
-
-/**
- * Object
- */
-const cube = new THREE.Mesh(
-    new THREE.BoxGeometry(0.25, 1, 0.25),
-    new THREE.MeshBasicMaterial()
-)
-cube.position.x = -2
-
-const cube2 = new THREE.Mesh(
-    new THREE.BoxGeometry(0.25, 1, 0.25),
-    new THREE.MeshBasicMaterial()
-)
-cube2.position.x = 2
-
-const ball = new THREE.Mesh(
-    new THREE.SphereGeometry(0.2, 16, 16),
-    new THREE.MeshNormalMaterial()
-)
-
-cube.material.color =  new THREE.Color('red')
-cube2.material.color = new THREE.Color('blue')
-//ball.material.color = new THREE.Color('yellow')
-
-scene.add(cube, cube2, ball)
-
-/**
- * Sizes
- */
-// const sizes = {
-//     width: window.innerWidth * .8,
-//     height: window.innerHeight / 2
-// }
-const sizes = { width: 0, height: 0 }
-function setWinSizes() {
-    sizes.width = window.innerWidth
-    sizes.height = window.innerHeight
-}
-setWinSizes()
-
-window.addEventListener('resize', () =>
-{
-    // Update sizes
-    setWinSizes()
-
-    // Update camera
-    //camera.aspect = sizes.width / sizes.height
-    //camera.updateProjectionMatrix()
-
-    // Update renderer
-    renderer.setSize(sizes.width, sizes.height)
-    renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+// Resize listener
+window.addEventListener('resize', () => {
+    t.setWinSizes()
+    t.renderer.setSize(t.sizes.width, t.sizes.height)
+    t.renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
 })
 
-/**
- * Camera
- */
-// Base camera
-const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 100)
-//const camera = new THREE.PerspectiveCamera(75, 1, 0.1, 100)
-camera.position.x = 0
-camera.position.y = 0
-camera.position.z = 4
-scene.add(camera)
+// Camera setups
+t.camera.position.z = 5
+t.scene.add(t.camera)
+t.gcamera.position.set(4, 4, 4)
+t.gcamera.rotation.y = Math.PI / 2
+t.gcamera.lookAt(t.scene.position)
+t.scene.add(t.gcamera)
 
-// Controls
-const controls = new OrbitControls(camera, canvas)
-controls.enableDamping = true
-
-/**
- * Renderer
- */
-const renderer = new THREE.WebGLRenderer({
-    canvas: canvas
-})
-renderer.setSize(sizes.width, sizes.height)
-renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
-
-// Camera rotation values
-let angle = 0; // Start angle
-const radius = 5; // Distance from origin
-const speed = 0.01; // Rotation speed
-
-/**
- * Animate
- */
-const clock = new THREE.Clock()
-
-const tick = () =>
-{
-    const elapsedTime = clock.getElapsedTime()
-
-    // Update camera position
-    angle += speed;
-    camera.position.x = radius * Math.cos(angle);
-    camera.position.z = radius * Math.sin(angle);
-    camera.lookAt(scene.position); // Assuming the origin is at (0, 0, 0)
-
-
-    // Update controls
-    controls.update()
-
-    // Render
-    renderer.render(scene, camera)
-
-    // Call tick again on the next frame
+// Frame tick
+const tick = () => {
+    m.tick()
+    m.controls.update()
+    t.renderer.render(t.scene, m.camera)
     window.requestAnimationFrame(tick)
 }
-
 tick()
+
+var gametype = -1
+export async function loadGame(nbr, nameLeft, nameRight) {
+    gametype = nbr
+    if (nbr === 0) return `
+     <button class="btn btn-primary btn-lg mb-3" id="begin-solo-match" translate="begin"></button>
+            <p class="w-100" style="color: white;" translate="instruction solo"></p>
+            </div>`;
+    if (nbr === 1) return `
+    <button type="button" id="start-button" class="btn btn-link" translate="start"></button>
+    <p class="w-100" style="color: white; id="instruction-yellow"><span>${nameLeft}</span><span translate="instruction yellow"></span></p></p>
+    <p class="w-100" style="color: white;" id="instruction-purple" translate="instruction purple"></p>
+ 
+    <form id="name-red">
+        <div class="form-group">
+            <input type="text" id="opp-name" class="form-control" required>
+        </div>
+    <button type="submit" class="btn btn-success mt-3" id="opp-name-submit" translate="set name"></button>
+    </form>
+    `;
+    if (nbr === 2)
+    {
+        try
+        {
+            const response = await fetch(`https://localhost:1443/api/profile/`, {
+                method: 'GET',
+                credentials: 'include'
+            });
+            if (!response.ok)
+            {
+                console.error('Failed getting display name:', response.statusText);
+                showAlert('Error occurred getting display name. Try again.', 'danger');
+                loadContent('home');
+                return ;
+            }
+            const user = await response.json();
+
+            return `
+            <p style="color: white;"><span>${user.display_name} </span><span translate="4p p1"></span></p>
+            <p class="w-100" style="color: white;" id="instruction-blue-4" translate="instruction blue 4"></p>
+            <p class="w-100" style="color: white;" id="instruction-purple-4" translate="instruction purple 4"></p>
+            <p class="w-100" style="color: white;" id="instruction-red-4" translate="instruction red 4"></p>
+            <form id="playerSelectForm">
+            
+            <label for="username1" style="color: white;" translate="4p p2"></label><br>
+            <input type="text" id="username1" name="username1" list="players"><br>
+            
+            <label for="username2" style="color: white;" translate="4p p3"></label><br>
+            <input type="text" id="username2" name="username2" list="players"><br>
+            
+            <label for="username3" style="color: white;" translate="4p p4"></label><br>
+            <input type="text" id="username3" name="username3" list="players"><br>
+            
+            <datalist id="players" style="color: white;">
+                <option value="Player1">
+                <option value="Player2">
+                <option value="Player3">
+                <!-- Add more options as needed -->
+            </datalist><br>
+            
+            <input type="submit" value="Start">
+            </form>
+            `;
+        }
+        catch (error)
+        {
+            console.log(error);
+            loadContent('home');
+            return ; // ?edit to be like tournament ?
+        }
+
+    }
+    if (nbr == 3)
+    {
+        try
+        {
+            const response = await fetch(`https://localhost:1443/api/get_display_name/?username=${nameLeft}`, {
+                method: 'GET',
+                credentials: 'include'
+            });
+            if (!response.ok)
+            {
+                console.error('Failed getting display name:', response.statusText);
+                showAlert('Error occurred getting display name. Try again.', 'danger');
+                loadContent('home');
+                return ;
+            }
+            const left = await response.json();
+
+            const reply = await fetch(`https://localhost:1443/api/get_display_name/?username=${nameRight}`, {
+                method: 'GET',
+                credentials: 'include'
+            });
+            if (!reply.ok)
+            {
+                console.error('Failed getting display name:', response.statusText);
+                showAlert('Error occurred getting display name. Try again.', 'danger');
+                
+            }
+            const right = await reply.json();
+
+            return `
+            <button class="btn btn-primary btn-lg mb-3" id="begin-tourney-match" translate="begin"></button>
+            <p class="w-100" style="color: white;">
+                <span>${left.display_name} </span>
+                <span translate="instruction tournament p1"></span>
+            </p>
+            <p class="w-100" style="color: white;">
+                <span>${right.display_name} </span>
+                <span translate="instruction tournament p2"><span/>
+            </p>
+            </div>
+            `;
+        }
+        catch (error)
+        {
+            console.log(error);
+            loadContent('home');
+        }
+
+    }
+    return ``
+}
+
+export function startGame(gametype, nameLeft, nameRight) {
+    if (gametype === 0) startQuickGame()
+    else if (gametype === 1) startTwoLocal()
+    else if (gametype === 2) startFourLocal()
+    else if (gametype === 3) startTourney(nameLeft, nameRight)
+    gametype = -1
+}
+
+export function endGame() {
+    gametype = -1
+}
+
+export function sendResults(scoreLeft, scoreRight, oppIsHuman, oppName) {
+    try {
+        console.log('result', scoreLeft, scoreRight, oppIsHuman, "", oppName);
+        if (scoreLeft > scoreRight)
+            showAlert('Player on the left wins', 'warning');
+        else
+            showAlert('Player on the right wins', 'warning');
+        loadContent('result', scoreLeft, scoreRight, oppIsHuman, "", oppName)
+    } catch (error) {
+        console.log('failed to sendResults:', error)
+    }
+}
+
+export function sendTourneyResults(scoreLeft, scoreRight, oppIsHuman, nameLeft, nameRight) {
+    try {
+        loadContent('tourneyResult', scoreLeft, scoreRight, oppIsHuman, nameLeft, nameRight)
+    } catch (error) {
+        console.log('failed to sendResults:', error)
+    }
+}
+
+
+export function switchToIdle() {
+    m.cleanUp()
+    m = i
+    m.addIdleObjs()
+}
+
+// external functions
+export function startQuickGame() {
+    m.cleanUp()
+    m = s
+    m.startQuickGame()
+}
+
+export function startTwoLocal() {
+    m.cleanUp()
+    m = l2
+    m.startGame(0)
+}
+
+export function startTourney(nameLeft, nameRight) {
+    m.cleanUp()
+    m = l2
+    m.startGame(1, nameLeft, nameRight)
+}
+
+function startFourLocal() {
+    console.log("Start 4");
+    m.cleanUp()
+    m = l4
+    m.startGame()
+}
