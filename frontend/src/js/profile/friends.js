ebc79e747cadaa8e8b7a9e0d307afeade3572e4c
Taru Finni <120325749+tafinni@users.noreply.github.com>
2024-09-26 18:15:29 +0300

    Added try catch

--- frontend/src/js/profile/friends.js
+++ frontend/src/js/profile/friends.js
@@ -7,6 +7,83 @@ import { getCookie } from '../csrf.js';
 export async function displayFriends() {
 	try
 		{
+			const response = await fetch('https://localhost:1443/api/profile/',
+			{
+				method: 'GET',
+				credentials: 'include'
+			});
+
+			if (!response.ok)
+			{
+				console.error('Failed loading profile:', response.statusText);
+				showAlert('Error occured displaying friends. Try again.', 'danger');
+				return ;
+			}
+
+			const data = await response.json();
+			const friends = data.friends;
+
+			const friendsListContainer = document.getElementById('friends-list');
+			friendsListContainer.innerHTML = '';
+
+		friends.forEach((friend, index) => {
+			const colors = ['#f0f0f0', '#ffffff'];
+
+				const friendItem = document.createElement('div');
+				friendItem.className = 'friend-item';
+
+				friendItem.style.backgroundColor = colors[index % colors.length];
+				friendItem.style.padding = '10px';
+				friendItem.style.display = 'flex';
+				friendItem.style.alignItems = 'center';
+
+				const onlineStatus = document.createElement('span');
+				onlineStatus.className = 'online-status';
+				onlineStatus.style.marginRight = '10px';
+				onlineStatus.style.fontSize = '80%';
+
+				if (friend.online_status == true)
+				{
+					onlineStatus.innerHTML = '<i class="bi bi-circle-fill"></i>';
+					onlineStatus.style.color = 'green';
+				}
+				else if (friend.online_status == false)
+				{
+					onlineStatus.innerHTML = '<i class="bi bi-circle-fill"></i>';
+					onlineStatus.style.color = 'red';
+				}
+
+				const friendNameButton = document.createElement('button');
+				friendNameButton.className = 'btn btn-link';
+				friendNameButton.style.transform = 'scale(1.3)';
+				friendNameButton.textContent = friend.username;
+				friendNameButton.onclick = () => loadPublicProfile(friend.username);
+
+				const removeFriendButton = document.createElement('button');
+				removeFriendButton.className = 'btn btn-link btn-sm';
+				removeFriendButton.innerHTML = '<i class="bi bi-trash3-fill"></i>';
+				removeFriendButton.style.marginRight = '10px';
+				removeFriendButton.style.marginLeft = 'auto';
+				removeFriendButton.style.color = 'red';
+				removeFriendButton.style.fontSize = '110%';
+				removeFriendButton.onclick = () => removeFriend(friend.username);
+
+				friendItem.appendChild(onlineStatus);
+				friendItem.appendChild(friendNameButton);
+				friendItem.appendChild(removeFriendButton);
+
+				friendsListContainer.appendChild(friendItem);
+			});
+	}
+	catch (error)
+	{
+		console.log(error);
+	}
+}
+
+export async function displayFriendRequests() {
+	try
+	{
 		const response = await fetch('https://localhost:1443/api/profile/',
 		{
 			method: 'GET',
@@ -16,61 +93,49 @@ export async function displayFriends() {
 		if (!response.ok)
 		{
 			console.error('Failed loading profile:', response.statusText);
-			showAlert('Error occured displaying friends. Try again.', 'danger');
+			showAlert('Error occured displaying friend requests. Try again.', 'danger');
 			return ;
 		}
-
 		const data = await response.json();
-		const friends = data.friends;
 
-		const friendsListContainer = document.getElementById('friends-list');
+		const friends = data.friend_requests;
+
+		const friendsListContainer = document.getElementById('friend-requests');
 		friendsListContainer.innerHTML = '';
 
-	friends.forEach((friend, index) => {
-		const colors = ['#f0f0f0', '#ffffff'];
+		friends.forEach((friend, index) => {
+			const colors = ['#f0f0f0', '#ffffff'];
 
 			const friendItem = document.createElement('div');
 			friendItem.className = 'friend-item';
+			friendItem.style.display = 'flex';
+			friendItem.style.alignItems = 'center';
+			friendItem.style.marginBottom = '10px';
 
 			friendItem.style.backgroundColor = colors[index % colors.length];
 			friendItem.style.padding = '10px';
-			friendItem.style.display = 'flex';
-			friendItem.style.alignItems = 'center';
 
-			const onlineStatus = document.createElement('span');
-			onlineStatus.className = 'online-status';
-			onlineStatus.style.marginRight = '10px';
-			onlineStatus.style.fontSize = '80%';
+			const friendName = document.createElement('span');
+			friendName.className = 'friend-name';
+			friendName.textContent = friend.username;
+			friendName.style.flexGrow = '1';
 
-			if (friend.online_status == true)
-			{
-				onlineStatus.innerHTML = '<i class="bi bi-circle-fill"></i>';
-				onlineStatus.style.color = 'green';
-			}
-			else if (friend.online_status == false)
-			{
-				onlineStatus.innerHTML = '<i class="bi bi-circle-fill"></i>';
-				onlineStatus.style.color = 'red';
-			}
+			// Create Accept button
+			const acceptButton = document.createElement('button');
+			acceptButton.className = 'btn btn-success btn-sm';
+			acceptButton.innerHTML = '<i class="bi bi-check-lg"></i>';
+			acceptButton.style.marginRight = '10px';
+			acceptButton.onclick = () => acceptFriend(friend.username);
 
-			const friendNameButton = document.createElement('button');
-			friendNameButton.className = 'btn btn-link';
-			friendNameButton.style.transform = 'scale(1.3)';
-			friendNameButton.textContent = friend.username;
-			friendNameButton.onclick = () => loadPublicProfile(friend.username);
+			// Create Deny button
+			const denyButton = document.createElement('button');
+			denyButton.className = 'btn btn-danger btn-sm';
+			denyButton.innerHTML = '<i class="bi bi-x-lg"></i>';
+			denyButton.onclick = () => declineFriend(friend.username);
 
-			const removeFriendButton = document.createElement('button');
-			removeFriendButton.className = 'btn btn-link btn-sm';
-			removeFriendButton.innerHTML = '<i class="bi bi-trash3-fill"></i>';
-			removeFriendButton.style.marginRight = '10px';
-			removeFriendButton.style.marginLeft = 'auto';
-			removeFriendButton.style.color = 'red';
-			removeFriendButton.style.fontSize = '110%';
-			removeFriendButton.onclick = () => removeFriend(friend.username);
-
-			friendItem.appendChild(onlineStatus);
-			friendItem.appendChild(friendNameButton);
-			friendItem.appendChild(removeFriendButton);
+			friendItem.appendChild(friendName);
+			friendItem.appendChild(acceptButton);
+			friendItem.appendChild(denyButton);		
 
 			friendsListContainer.appendChild(friendItem);
 		});
@@ -79,65 +144,6 @@ export async function displayFriends() {
 	{
 		console.log(error);
 	}
-}
-
-export async function displayFriendRequests() {
-	
-	const response = await fetch('https://localhost:1443/api/profile/',
-	{
-		method: 'GET',
-		credentials: 'include'
-	});
-
-	if (!response.ok)
-	{
-		console.error('Failed loading profile:', response.statusText);
-		showAlert('Error occured displaying friend requests. Try again.', 'danger');
-		return ;
-	}
-	const data = await response.json();
-
-	const friends = data.friend_requests;
-
-	const friendsListContainer = document.getElementById('friend-requests');
-	friendsListContainer.innerHTML = '';
-
-	friends.forEach((friend, index) => {
-		const colors = ['#f0f0f0', '#ffffff'];
-
-		const friendItem = document.createElement('div');
-		friendItem.className = 'friend-item';
-		friendItem.style.display = 'flex';
-        friendItem.style.alignItems = 'center';
-        friendItem.style.marginBottom = '10px';
-
-		friendItem.style.backgroundColor = colors[index % colors.length];
-		friendItem.style.padding = '10px';
-
-		const friendName = document.createElement('span');
-		friendName.className = 'friend-name';
-		friendName.textContent = friend.username;
-        friendName.style.flexGrow = '1';
-
-		// Create Accept button
-        const acceptButton = document.createElement('button');
-        acceptButton.className = 'btn btn-success btn-sm';
-		acceptButton.innerHTML = '<i class="bi bi-check-lg"></i>';
-	    acceptButton.style.marginRight = '10px';
-        acceptButton.onclick = () => acceptFriend(friend.username);
-
-        // Create Deny button
-        const denyButton = document.createElement('button');
-        denyButton.className = 'btn btn-danger btn-sm';
-		denyButton.innerHTML = '<i class="bi bi-x-lg"></i>';
-        denyButton.onclick = () => declineFriend(friend.username);
-
-		friendItem.appendChild(friendName);
-        friendItem.appendChild(acceptButton);
-        friendItem.appendChild(denyButton);		
-
-		friendsListContainer.appendChild(friendItem);
-	});	
 }
 
 async function acceptFriend(request_user_username) {
@@ -211,42 +217,49 @@ async function declineFriend(request_user_username) {
 }
 
 export async function addFriend() {
-	const response = await fetch('https://localhost:1443/api/profile/', {
-		method: 'GET',
-		credentials: 'include'
-	});
-	if (!response.ok)
+	try
 	{
-		console.error('Failed loading profile:', response.statusText);
-		showAlert('Error occured adding friend. Try again.', 'danger');
-		return ;
-	}
+		const response = await fetch('https://localhost:1443/api/profile/', {
+			method: 'GET',
+			credentials: 'include'
+		});
+		if (!response.ok)
+		{
+			console.error('Failed loading profile:', response.statusText);
+			showAlert('Error occured adding friend. Try again.', 'danger');
+			return ;
+		}
 
-	const addFriendHTML = `
-		<div class="container mt-5">
-			<div class="card" style="background-color: white; padding: 20px; border-radius: 10px;">
-			<form id="add-friends-form">
-				<div class="form-group">
-					<label for="friends-name" translate="friend's name"></label>
-					<input type="text" id="new-friend" class="form-control" required>
+		const addFriendHTML = `
+			<div class="container mt-5">
+				<div class="card" style="background-color: white; padding: 20px; border-radius: 10px;">
+				<form id="add-friends-form">
+					<div class="form-group">
+						<label for="friends-name" translate="friend's name"></label>
+						<input type="text" id="new-friend" class="form-control" required>
+					</div>
+					<button type="submit" class="btn btn-success mt-3" translate="send request"></button>
+					<button type="button" id="cancel-button" class="btn btn-link" translate="cancel"></button>
+				</form>
 				</div>
-				<button type="submit" class="btn btn-success mt-3" translate="send request"></button>
-				<button type="button" id="cancel-button" class="btn btn-link" translate="cancel"></button>
-			</form>
 			</div>
-		</div>
-	`;
+		`;
 
 
-	const contentElement = document.getElementById('content');
-	if (contentElement)
-	{
-		contentElement.innerHTML = addFriendHTML;
-		updateContent();
-		saveFriend();
+		const contentElement = document.getElementById('content');
+		if (contentElement)
+		{
+			contentElement.innerHTML = addFriendHTML;
+			updateContent();
+			saveFriend();
+		}
+		else
+			console.error('Content element not found');
 	}
-	else
-		console.error('Content element not found');
+	catch (error)
+	{
+		console.log(error);
+	}
 }
 
 
