e04b4d0a48fd4cf779bdb6644b8be27c486c33a1
Taru Finni <120325749+tafinni@users.noreply.github.com>
2024-11-07 17:24:28 +0200

    Updated variable

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -111,7 +111,7 @@ DATABASES = {
         'NAME': os.getenv('DATABASE_NAME'),
         'USER': os.getenv('DATABASE_USER'),
         'PASSWORD': os.getenv('DATABASE_PASSWORD'),
-        'HOST': os.getenv('DATABASE_HOST'),
+        'HOST': 'db',
         'PORT': '5432',
     }
 }
